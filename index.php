0df4d7aef769fc4faf15f92e6f5c28ad874f4435
Lorefrance Ubay <FrankenState>
2019-09-19 20:03:34 +0800

    Saving last state.

--- index.php
+++ index.php
@@ -20,13 +20,41 @@ include 'sys/db.php';
 	$result = $conn->query($sql);
 
 	//var_dump($result);
-
+	// date_format(date_create($birthDate)
 	if ($result->num_rows > 0){
+	?>
+	<table class="table table-sm table-bordered">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>Birth Date</th>
+			<th>Gender</th>
+			<th>Expertise</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php
 		while ($row = $result->fetch_assoc()){
-			#echo "<br/>{$row['name']}";
-			echo "<br/>".$row['name'];
-		}
-	}
+	?>
+			<tr>
+				<td><?php echo $row['id']; ?></td>
+				<td><?php echo $row['name']; ?></td>
+				<td><?php echo $row['gender']; ?></td>
+				<td><?php echo date_format(date_create($row['birthdate']), "F d, Y"); ?></td>
+				<td><?php echo $row['expertise']; ?></td>
+			</tr>
+	<?php
+		} // end of while
+	?>
+	</tbody>
+	</table>
+	<?php
+	} // end of if
+
+	echo "<br/>". date("F m, Y");
+	$tz=timezone_open("Europe/Paris");
+	echo "<br/>". timezone_name_get($tz);
 ?>
 
 	</div>
