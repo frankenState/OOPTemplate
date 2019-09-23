d86ab8e125ad794f70f8ad0582bca26f785501c8
Lorefrance Ubay <FrankenState>
2019-09-23 18:47:22 +0800

    Saving last state.

---
+++ demo.php
@@ -0,0 +1,24 @@
+<?php 
+$nums = array(1.9,300,400,398,2);
+
+rsort($nums);
+
+// var_dump($nums);
+
+$names = [
+	"last" => "Black",
+	"first" => "James",
+	"middle" => "White"
+];
+
+// asort($names);
+// ksort($names);
+// arsort($names);
+// krsort($names);
+//var_dump($names);
+
+echo date('F d, Y') . "<br/>";
+echo date('Y/m/d') . "<br/>";
+echo date('Y/m/D') . "<br/>";
+
+?>
